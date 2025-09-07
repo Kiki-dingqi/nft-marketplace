a1982332e90c0235575ad3302df1a5a60877e73c
Kiki-dingqi <dingqilove163@163.com>
2025-09-07 15:18:04 +0800

    fix:fix baselayout style

--- components/ui/layout/BaseLayout.tsx
+++ components/ui/layout/BaseLayout.tsx
@@ -5,7 +5,7 @@ const BaseLayout:FunctionComponent<PropsWithChildren> = ({children}) => {
         <>
             <Navbar/>
             <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
-                <div>
+                <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
                     {children}
                 </div>
             </div>
